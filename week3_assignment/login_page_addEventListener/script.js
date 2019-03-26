0e52e676c581e8acff9e688c6c2b6f9239068e57
Deepak Shrivastava <dpkshrivastava10@gmail.com>
2019-03-26 23:58:11 +0530

    Validate using addEventListener

---
+++ week3_assignment/login_page_addEventListener/script.js
@@ -0,0 +1,29 @@
+'use strict'
+
+var max_attempt = 3;
+
+function validate() {
+  
+var username= document.getElementById("username").value;
+var password = document.getElementById("password").value;
+
+console.log(username, password);
+
+if( username ==="deepak" && password === "shrivastava"){
+
+    alert("login Successfully");
+    window.location= "login.html"; //redirect to the same page again.
+ 
+}
+else{
+alert ("Login Failed...");
+max_attempt--;
+}
+
+if(max_attempt==0){
+
+    alert("You have exceeded the maximum limit:");
+
+}
+}
+document.getElementById("submit").addEventListener("click",validate); 
